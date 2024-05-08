2b0e4cfb5400a3598015c23f179bf31cf92dcba7
Muhammad Uwais <163686553+Uwais245@users.noreply.github.com>
2024-05-08 20:31:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "kindly enter your first no: "
+    },
+    { type: "number",
+        name: "numberTwo",
+        message: "kindly enter your second no: "
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+", "-", "/"],
+        message: "select Operator: "
+    },
+]);
+const { numberOne, numberTwo, operator } = answer;
+if (numberOne && numberTwo && operator) {
+    let result = 0;
+    if (operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    if (operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    if (operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    console.log("Your result is :", result);
+}
+else {
+    console.log("Kindly enter valid input");
+}
